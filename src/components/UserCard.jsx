f2852eb1fe39fe7a1d0470bd3e07dcf9ca3dbb79
lucasb <lucasbarcelo1981@gmail.com>
2023-09-04 17:01:38 -0300

    node backend

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,39 +1,56 @@
-import './styles/UserCard.css'
+import "./styles/UserCard.css";
 
+const UserCard = ({ user, deleteUsersById, setUpdateInfo, handleOpenForm }) => {
+  const handleDelete = () => {
+    deleteUsersById("", user.id);
+  };
 
-const UserCard = ({user, deleteUsersById, setUpdateInfo, handleOpenForm}) => {
+  const handleUpdate = () => {
+    setUpdateInfo(user);
+    handleOpenForm();
+  };
 
-    const handleDelete = () => {
-        deleteUsersById('', user.id)
-    }
+  return (
+    <div className="card__article">
+      <h2>{`${user.first_name} ${user.last_name}`}</h2>
+      <hr />
+      <ul>
+        <li>
+          <span className="span1">
+            First Name <i className='bx bx-user'></i>{" "}
+          </span>
+          <span className="span2">{user.first_name}</span>
+        </li>
+        <li>
+          <span className="span1">
+            Last Name <i className='bx bxs-user'></i>{" "}
+          </span>
+          <span className="span2">{user.last_name}</span>
+        </li>
+        <li>
+          <span className="span1">
+            Email <i className="bx bx-envelope"></i>{" "}
+          </span>
+          <span className="span2">{user.email}</span>
+        </li>
+        <li>
+          <span className="span1">
+            Birthday <i className="bx bx-gift"></i>{" "}
+          </span>
+          <span className="span2">{user.birthday}</span>
+        </li>
+      </ul>
+      <hr />
+      <footer className="footer_usercard">
+        <button onClick={handleDelete}>
+          <i className="bx bx-trash"></i>
+        </button>
+        <button onClick={handleUpdate}>
+          <i className="bx bx-edit"></i>
+        </button>
+      </footer>
+    </div>
+  );
+};
 
-    const handleUpdate = () => {
-        setUpdateInfo(user)
-        handleOpenForm()
-    }
-
-
-    return(
-        <div className="card__article">
-            <h2>{`${user.first_name} ${user.last_name}`}</h2>
-            <hr />
-            <ul>
-                <li>
-                    <span className='span1'>Email <i className='bx bx-envelope'></i> </span>
-                    <span className='span2'>{user.email}</span>
-                </li>
-                <li>
-                    <span className='span1'>Birthday <i className='bx bx-gift'></i> </span>
-                    <span className='span2'>{user.birthday}</span>
-                </li>
-            </ul>
-            <hr />
-            <footer>
-                <button onClick={handleDelete}><i className='bx bx-trash'></i></button>
-                <button onClick={handleUpdate}><i className='bx bx-edit' ></i></button>
-            </footer>
-        </div>
-    )
-}
-
-export default UserCard
+export default UserCard;
